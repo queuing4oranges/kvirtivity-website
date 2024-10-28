d4f5ab16cf0a514eb02a79f469d45bf8accb4bac
queuing4oranges <katjazenker@gmail.com>
2024-10-28 13:08:52 +0100

    Initial commit

---
+++ kvirtivity/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import { Button } from 'reactstrap';
+
+function App() {
+
+
+	return (
+		<>
+			<h1>Hello World</h1>
+			<button className='btn btn-danger'>Danger</button>
+			<Button color='info'>Info</Button>
+		</>
+	)
+}
+
+export default App
