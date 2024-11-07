27d19b58b6275a8daae99614d69fe21052a4c00a
queuing4oranges <katjazenker@gmail.com>
2024-11-07 22:35:53 +0100

    Fix language switcher to default to CZ

--- src/LanguageSwitcher.jsx
+++ src/LanguageSwitcher.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import './languageswitcher.scss';
 
+// Component to toggle between languages
 export default function LanguageSwitcher() {
 	const { i18n } = useTranslation();
 	const [language, setLanguage] = useState('cz');
 	
+	// Set initial language to Czech if not already set
+	useEffect(() => {
+		if (i18n.language !== language) {
+			i18n.changeLanguage(language);
+		}
+	})
+
 	const changeLanguage = (lang) => {
 		setLanguage(lang);
 		i18n.changeLanguage(lang);
