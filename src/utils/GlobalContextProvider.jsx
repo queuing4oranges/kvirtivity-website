c3e734c653a568dedb5f0e657735a365ef941153
queuing4oranges <katjazenker@gmail.com>
2024-11-15 22:57:51 +0100

    Minor fixes

--- src/utils/GlobalContextProvider.jsx
+++ src/utils/GlobalContextProvider.jsx
@@ -3,10 +3,21 @@ import React, { createContext, useState } from 'react';
 export const GlobalContext = createContext();
 
 const GlobalContextProvider = ({ children }) => {
+	const [events, setEvents] = useState([]);
 	const [showSingleEvent, setShowSingleEvent] = useState(false);
+	const [singleEventId, setSingleEventId] = useState(null);
 	
 	return (
-		<GlobalContext.Provider value={{showSingleEvent, setShowSingleEvent}}>
+		<GlobalContext.Provider
+			value={{
+				events,
+				setEvents,
+				showSingleEvent,
+				setShowSingleEvent,
+				singleEventId,
+				setSingleEventId
+			}}
+		>
 			{children}
 		</GlobalContext.Provider>
 	);
