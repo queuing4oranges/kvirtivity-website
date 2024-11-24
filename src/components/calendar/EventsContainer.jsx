25b983513aa11ce03e01b1e9b6c1bc85433f277d
queuing4oranges <katjazenker@gmail.com>
2024-11-24 12:31:21 +0100

    Move date functions into utils

--- src/components/calendar/EventsContainer.jsx
+++ src/components/calendar/EventsContainer.jsx
@@ -1,56 +1,19 @@
 import React, { useContext } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Card, CardImg, CardBody } from 'reactstrap';
 
 import { API_BASE_URL } from '../../api';
 import { GlobalContext } from '../../utils/GlobalContextProvider';
+import { getDay, getMonthName } from '../../utils/dateConverter';
 
 import './eventscontainer.scss';
 
 //TODO make alternative image if image is broken or missing
 
 export default function EventsContainer() {
-	const { t } = useTranslation();
 	const { events,
 		showSingleEvent, setShowSingleEvent,
 		setSingleEventId 
 	} = useContext(GlobalContext);
-
-	//TODO make all date/time related functions into util component
-	// Get the abbreviated name of the month and localize it
-	const getMonthName = (datestring) => {
-		// Check if the datestring is a valid string
-		if (typeof datestring !== 'string' || !datestring.trim()) {
-			return '';
-		}
-		
-		// Check if the datestring is in a valid date format ('YYYY-MM-DD')
-		const date = new Date(datestring);
-		if (isNaN(date.getTime())) {
-			return ''; // Return empty string for invalid date format
-  		}
-		
-		const months = [
-			t('Months.Led'), t('Months.Úno'), t('Months.Bře'), t('Months.Dub'), t('Months.Kvě'), t('Months.Čvn'),
-			t('Months.Čvc'), t('Months.Srp'), t('Months.Zář'), t('Months.Říj'), t('Months.Lis'), t('Months.Pro')
-		]
-		
-		// Extract the month part from the date string
-		const monthNumber = parseInt(datestring.split('-')[1], 10); // Convert 02 to 2
-		
-		return months[monthNumber - 1] // Adjust for zero-based index
-	};
-	
-	// Get the day of the event date
-	const getDay = (datestring) => {
-		// Check if the datestring is a valid string
-		if (typeof datestring !== 'string' || !datestring.trim()) {
-			return '';
-		}
-
-		const date = new Date(datestring);
-		return date.getDay()
-	};
 
 	return (
 		<>
