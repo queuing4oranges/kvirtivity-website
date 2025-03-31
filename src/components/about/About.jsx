9f2fa055d2ddd81b1867c668fda9fcb03268dc1f
queuing4oranges <katjazenker@gmail.com>
2025-03-31 15:01:36 +0200

    Refine about texts

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -28,15 +28,28 @@ export default function About() {
 		<Container fluid className='min-vh-100 p-5 d-flex flex-column'>
 			<Row style={{ marginTop: '3rem' }}><h1>{t('O Nás')}</h1></Row>
 			<Row className='w-100'>
-				<Col md='4'>
-				<p>
-					<strong>Hi, we are Kvitivity...</strong> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Aenean placerat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Phasellus et lorem id felis nonummy placerat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Etiam commodo dui eget wisi. Aenean vel massa quis mauris vehicula lacinia.
-				</p>
-				<p>
-					{activeText}
-				</p>
+				<Col md='5' className='d-flex'>
+					<Col md='6' className='p-5'>
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Aenean placerat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Phasellus et lorem id felis nonummy placerat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Etiam commodo dui eget wisi. Aenean vel massa quis mauris vehicula lacinia.
+						</p>
+					</Col>
+					<Col className='p-5 mt-5'>
+						{!activeText ?
+							<span className='h-100 mt-5'>
+								<h3>We are Kvirtivity</h3>
+							</span>
+						:
+							<span>
+								<p>
+									<strong>Hi, I'm{activeText.name}</strong>
+								</p>
+								<p>{activeText.intro}</p>
+							</span>
+						}
+					</Col>
 				</Col>
-				<Col md='8'>
+				<Col md='7' className='d-flex justify-content-center'>
 					<div className='d-flex align-items-start justify-content-end'>
 						<svg width='750' height='750' viewBox='120 120 720 720' fill='none' xmlns='http://www.w3.org/2000/svg'>
 							<defs>
@@ -53,14 +66,14 @@ export default function About() {
 							</defs>
 							{/* Blue Path */}
 							<path
-								onMouseEnter={() => handleMouseEnter(aboutData[0].intro)}
+								onMouseEnter={() => handleMouseEnter(aboutData[0])}
 								onMouseLeave={handleMouseLeave}
 								className='path-blue'
 								d='M836.85 502.024C842.371 441.38 831.974 380.345 806.687 324.949C781.399 269.553 742.096 221.713 692.66 186.155C643.225 150.598 585.368 128.554 524.805 122.201C464.242 115.848 403.07 125.407 347.332 149.931L488.291 470.291L836.85 502.024Z'
 								fill='#00FFFF'
 							/>
 							<image
-								onMouseEnter={() => handleMouseEnter(aboutData[0].intro)}
+								onMouseEnter={() => handleMouseEnter(aboutData[0])}
 								onMouseLeave={handleMouseLeave}
 								href={marta}
 								x='500' y='210' width='200' height='200'
@@ -68,14 +81,14 @@ export default function About() {
 							/>
 							{/* Pink Path */}
 							<path
-								onMouseEnter={() => handleMouseEnter(aboutData[1].intro)}
+								onMouseEnter={() => handleMouseEnter(aboutData[1])}
 								onMouseLeave={handleMouseLeave}
 								className='path-pink'
 								d='M284.266 772.72C334.174 808.569 392.64 830.646 453.786 836.733C514.932 842.819 576.602 832.701 632.597 807.394C688.592 782.087 736.938 742.485 772.774 692.568C808.609 642.651 830.671 584.179 836.742 523.032L488.454 488.454L284.266 772.72Z'
 								fill='#FF1493'
 							/>
 							<image
-								onMouseEnter={() => handleMouseEnter(aboutData[1].intro)}
+								onMouseEnter={() => handleMouseEnter(aboutData[1])}
 								onMouseLeave={handleMouseLeave}
 								href={person1}
 								x='450' y='570' width='200' height='200'
@@ -83,15 +96,15 @@ export default function About() {
 							/>
 							{/* Yellow Path */}
 							<path
-								onMouseEnter={() => handleMouseEnter(aboutData[2].intro)}
+								onMouseEnter={() => handleMouseEnter(aboutData[2])}
 								onMouseLeave={handleMouseLeave}
 								className='path-yellow'
 								d='M329.556 161.167C274.304 186.773 226.689 226.351 191.415 275.993C156.142 325.634 134.431 383.619 128.428 444.22C122.425 504.821 132.337 565.939 157.185 621.536C182.033 677.133 220.958 725.285 270.112 761.233L476.723 478.723L329.556 161.167Z'
 								fill='#FFFF33'
 							/>
 							<image
-								onMouseEnter={() => handleMouseEnter(aboutData[2].intro)}
-								onMouseLeave={handleMouseLeave} //TODO want blur also when hover over image!!
+								onMouseEnter={() => handleMouseEnter(aboutData[2])}
+								onMouseLeave={handleMouseLeave}
 								href={person2}
 								x='200' y='350' width='200' height='200'
 								clipPath='url(#clip-yellow)'
--- src/components/about/aboutData.js
+++ src/components/about/aboutData.js
@@ -1,14 +1,14 @@
 export const aboutData = [
     {
         name: 'Marta',
-        intro: 'Hi, I am Marta ... Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Aenean placerat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Phasellus et lorem id felis nonummy placerat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.'
+        intro: 'I love Germans, cooking, crafts, Excel tables and am known as a cat lady. I\'m the boss bitch here. My responsibility is also speed dating, book club and making everyone feel welcomed. I dont read books often, but I will sacrifice myself for the book club! In my free time, I like to annoy my friends and make bad jokes about dark chapters of the European history. Other than that I consider myself mentally stable.'
     },
     {
         name: 'Eliska',
-        intro: 'Hi, I am Eliska ... Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Aenean placerat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Phasellus et lorem id felis nonummy placerat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.'
+        intro: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Aenean placerat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Phasellus et lorem id felis nonummy placerat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.'
     },
     {
         name: 'Lenka',
-        intro: 'Hi, I am Lenka ... Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Aenean placerat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Phasellus et lorem id felis nonummy placerat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.'
+        intro: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Aenean placerat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Phasellus et lorem id felis nonummy placerat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.'
     }
 ]
